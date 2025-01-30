7f42c09dc29306710fa73e7b01e4334699daab4e
nanajulieaugustine <naje0004@stud.kea.dk>
2025-01-30 19:00:41 +0100

    return værdier

---
+++ returngreeting/script.js
@@ -0,0 +1,24 @@
+// function greeting(firstName) {
+//   return `Hello ${firstName}`;
+// }
+// const result = greeting("Nana");
+// console.log(result);
+
+// function greeting(firstName) {
+//   return `Hello ${firstName}`;
+// }
+// const personToGreet = "Nana";
+
+// const greetingToNana = greeting(personToGreet);
+
+// console.log(greetingToNana);
+// console.log(greeting("Klaus"));
+
+function greeting(firstName) {
+  return `Hello ${firstName}`;
+}
+function lengthOfStr(str) {
+  return str.length;
+}
+console.log(lengthOfStr(greeting("Klaus")));
+// const greetingToNana = greeting("Klaus");
