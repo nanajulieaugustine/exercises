09ac978b3e5e94ae5ad69b20061bc81472f6d313
nanajulieaugustine <naje0004@stud.kea.dk>
2025-02-04 10:21:11 +0100

    update m breadcrumbs

--- bredcrumbs/script.js
+++ bredcrumbs/script.js
@@ -8,16 +8,12 @@ document.querySelector("button").addEventListener("click", showBreadcrumbs);
 
 function showBreadcrumbs() {
   bc.forEach((vare) => {
-    console.log("vare", vare.name);
-    console.log("vare", vare.link);
     if (bc[bc.length - 1].name === vare.name) {
-      document.querySelector(
-        "ul"
-      ).innerHTML += `<li>&nbsp&nbsp/${vare.name}</li>`;
+      document.querySelector("ul").innerHTML += `<li>${vare.name}</li>`;
     } else {
       document.querySelector(
         "ul"
-      ).innerHTML += `<li>&nbsp&nbsp/<a href="${vare.link}">${vare.name}</a></li>`;
+      ).innerHTML += `<li><a href="${vare.link}">${vare.name}</a>&nbsp/&nbsp</li>`;
     }
   });
 }
