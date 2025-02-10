6ec9fa3eebc12d08494707aedc269988c88892b6
nanajulieaugustine <naje0004@stud.kea.dk>
2025-02-10 10:28:41 +0100

    inkl. klaus' eksempel

--- language_switcher/js.js
+++ language_switcher/js.js
@@ -14,6 +14,18 @@ const texts = {
   },
 };
 
+// let locale = "da";
+
+// document.querySelector("#select").addEventListener("change", languageChange);
+
+// function languageChange(evt) {
+//   locale = document.querySelector("#select").value;
+//   console.log("texts", texts[locale].texts);
+//   texts[locale].texts.forEach((element) => {
+//     document.querySelector(element.location).textContent = element.text;
+//   });
+// }
+
 document.querySelector(".danish").addEventListener("click", showDanish);
 
 function showDanish() {
